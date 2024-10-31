e6c8f5997967107f3425b8935b3ed86b80923dc4
Loyd Perry <loydperry215@gmail.com>
2024-10-31 13:33:15 +0000

    completed Python Foundation

---
+++ Unit3_Python/PythonFoundation.py
@@ -0,0 +1,35 @@
+# data types= basic building blocks of python programming
+
+# string = data represnted wrapped 
+# together in quotationn
+
+# integer = whole numbers.
+678
+# floats = decimal numbers
+687.99
+#boolean = true or false values
+True
+False
+
+# variables = containers for data
+# We create variables using the following steps
+# 1. create a variable name
+# 2. apply an equal sign
+# 3. give it a data type value
+
+user = "Loyd"
+message = 'Good Morning,'
+weather = "It is 60 degress and sunny."
+
+# we use print to see data in our terminal
+print(message + user + "." + weather)
+
+number1= 1200
+number 2= 800
+
+print(number1, number2)
+
+name = 'my name is Loyd Perry'
+activity = 'I am going to my job'
+time = 'I will do this at 4:00'
+print(name, activity, )
