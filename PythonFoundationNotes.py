787401e6aa091780c0cdd1a29eae732631fa24ce
Loyd Perry <loydperry215@gmail.com>
2024-11-13 14:35:51 +0000

    learned aboput different operators.

---
+++ PythonFoundationNotes.py
@@ -0,0 +1,5 @@
+homeAddress = '123 Main street'
+telephoneNumber= 1234124843
+telephoneBillPaid= False
+balance = 300.00
+cellularCode_byUser_state_reset
---
+++ Unit3_Python/operatorReview.py
@@ -0,0 +1,19 @@
+# Arithmetic operator - writing code that does math process
+
+# an example of a computer program that uses arithmatic is a bank app
+depositAmount = 50
+withdrawAmount= 25
+checkingAccount= 1000
+savingsAccount = 2000
+creditAmount = 1500
+
+print(depositAmount + checkingAccount)
+
+# Assignment operators- giving variables values. KEYWORD is assignment
+# anmy time you are using a variable, you are using the assignment operator
+
+teeShirt= 50.00
+
+# Comparision
+
+# Logical
