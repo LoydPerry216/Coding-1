f4fda9459678bb5c096cb95d44c117548d75f786
Loyd Perry <loydperry215@gmail.com>
2024-12-04 14:35:43 +0000

    learned about using conditional statements.

--- Unit3_Python/handsOnFunctions.py
+++ Unit3_Python/handsOnFunctions.py
@@ -29,12 +29,37 @@ def modifyMyName(name):
 #keywords to filler and create specific outcomes
 #based on data.
 
-def verifyAge(age):
-    if age > 17: and age < 20:
-        print('congrats! you can buy GTA VI')
-    elif age > 20:
-        print('Sorry, you need an adult to buy this game.')
-else:
-    print('Sorry, you need an adult to buy this game.')
+#def verifyAge(age):
+    #if age > 17: and age < 20:
+        #print('congrats! you can buy GTA VI')
+    #elif age > 20:
+        #print('Sorry, you need an adult to buy this game.')
+#else:
+    #print('Sorry, you need an adult to buy this game.')
 
-verifyAge(69)
+#verifyAge(69)
+
+#def hoursToMinutes(hour):
+    #print(hour*60)
+
+#hoursToMinutes(4)
+
+# Conditional Statements
+# if/ else keywords; gives us the ability to
+# control outcome and make decisions on data.
+
+#food expiration software is an example of
+# using conditional statements. If the food expires
+#ot needs tp ne thrown away, otherwise, or else
+#it can be eaten. 
+
+def foodExpiration(month, date, year):
+    expirationMonth= 12
+    expirationDate= 5
+    expirationYear= 2024
+    if date > expirationDate and year > expirationYear:
+        print('throw food away.')
+    else:
+        print('food still good.')
+
+foodExpiration(12, 8, 2024)
