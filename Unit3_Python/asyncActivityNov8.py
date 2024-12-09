351144ebf8c27efca6ce9cf0b9789120c580924e
Loyd Perry <loydperry215@gmail.com>
2024-12-09 14:11:21 +0000

    finshed async work

---
+++ Unit3_Python/asyncActivityNov8.py
@@ -0,0 +1,10 @@
+#def reverse_word(word):
+    #return word[::-1]
+
+#print(reverse_word('pet'))
+#print(reverse_word('book'))
+
+def state_landmark(state):
+    if state =='Pennsylvania':
+        return "A landmark in Pennsylvaina is Indpendence Hall, where the famous Declaration of Independence was signed"
+print(state_landmark('Pennsylvania'))
