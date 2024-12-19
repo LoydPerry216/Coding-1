80614dcb0465c917e364e6ece0f2f9f5c8b5eb04
Loyd Perry <loydperry215@gmail.com>
2024-12-19 14:35:44 +0000

    completed the extra credit.

---
+++ Unit3_Python/holidayExtraCredit.py
@@ -0,0 +1,16 @@
+def holidayPlanner(date):
+    if date == 12-21-24:
+        print('i am chilling today') 
+    elif date == 12-28-24:
+        print(' i am going to see my mother')
+    elif date == 1-12-25:
+        print('i am visiting my grandma')
+    elif date == 1-19-25:
+        print('i have a football game ')    
+    elif date == 1-25-25:
+        print('i am going to a concert')    
+    else:
+        print ('nothing is planned for this day')
+ 
+
+holidayPlanner(11-21-24)       
