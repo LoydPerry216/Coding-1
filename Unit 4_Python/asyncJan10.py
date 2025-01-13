94a7785f233df4f2e1774933a6bbb4b14802f1ed
Loyd Perry <loydperry215@gmail.com>
2025-01-13 14:10:42 +0000

    worked on async work.

---
+++ Unit 4_Python/asyncJan10.py
@@ -0,0 +1,38 @@
+variable = 'loyd'
+newlist =[1,2,34,55]
+produceFood =[0, 12.35, True, variable,newlist]
+
+produceFood = ['apples','garlic','bellpeppers','bananas','spinch']
+frozenFood = ['pizza','string beans','hashbrowns','Tv dinners','frozen fruit']
+
+
+numbers = [1,30,39,50,293,100]
+
+
+1
+
+50
+
+293
+
+
+def multiplyByNumber(x):
+    result = x*293
+    result = x*numbers[4]
+
+    print(result)
+
+multiplyByNumber(1)    
+
+
+
+
+
+
+
+
+def traveledPlaces():
+    place = input("wWhere have you traveled to?")
+    print(place)
+
+traveledPlaces()    
---
+++ Unit 4_Python/pythonLoops.py
@@ -0,0 +1,42 @@
+# Create a function that will determine what
+# level of education a college student is in,
+# based on the number of years they've been in
+# school.
+
+# undergrad 1 freshman, 2 sophmore, etc...
+# 5> masters degree, doctorate degree, etc...
+
+def titleBySchoolYear():
+    year = input('how many years of college do you have?')
+    if year == '1'
+        print
+    elif year == 2:
+        print('you are a sophmore in undergrad')
+    elif year == 3:
+        print('you are a junior in undergrad')                
+    elif year == 4:
+        print('you are a senior in undergrad')
+    elif year == 5 or year == 6:
+        print('you are in a masters program and in grad school')
+    elif year >= 7 and year <= 10:
+        print('you are in a doctorates program and in grad school')        
+    elif year >11:
+        print('yo you need to go and get a job. you been in school too long')
+    else      
+#gradeToTitle()
+
+# A list is data type for collecting
+# and organizing other data types.
+
+listB =[10,102,4904]
+
+listofData = ['words and characters' ,
+10,
+10.2324,
+True,
+False,
+list
+]
+print(listofData)
+
+collegeTitles= ['freshman','sophmore','junior','senior']
